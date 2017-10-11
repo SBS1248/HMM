3f73ec4df0cceede116e40f14775f6989c97082c
Lee <wkdgma91@naver.com>
2017-10-11 16:15:19 +0900

    123

--- src/main/java/com/kh/hmm/SocketHandler.java
+++ src/main/java/com/kh/hmm/SocketHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-
+												
 public class SocketHandler extends TextWebSocketHandler implements InitializingBean {
 
 	private final Logger logger = LogManager.getLogger(getClass());
